63bc28c850a856a9ceff2942040fdfd14396bcc9
Meghna Srivastava <meghna.s1906@gmail.com>
2018-03-27 19:22:17 +0200

    Added solutions

---
+++ exercises/02-PartiesList.jsx
@@ -0,0 +1,26 @@
+// var React = require("react");
+
+import React, {Component} from 'react';
+
+// We will create dynamic list of parties nearby. Let's see how to render
+// a list of items.
+//
+// Task #1: Add another party (or parties) to the list.
+// Task #2: Change `ul` HTML attribute `class` to 'parties-list'.
+
+class PartiesList extends Component {
+  // We can put DOM elements delivered by React just like HTML elements.
+  // Doesn't this code look familiar to you?
+  render() {
+    return (
+      <ul className="parties-list">
+        <li>Party at Aperture Laboratories</li>
+        <li>Raj's Birthday Bash</li>
+      </ul>
+    );
+  }
+  // Think about it: Why is that `className` attribute not called just `class`?
+  // Hint: Some words in JavaScript/JSX are reserved...
+}
+
+export default PartiesList;
